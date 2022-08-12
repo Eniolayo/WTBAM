57f3a17dacdfabc18a346aa764854f048b30d493
Eniolayo <eniolayodeji@gmail.com>
2022-08-12 21:10:46 +0100

    set up almost complete

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <p>heyy</p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, ullam!
+      </p>
+    </div>
+  );
+}
+
+export default App;
